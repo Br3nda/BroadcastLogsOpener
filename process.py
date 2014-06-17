d54476a1d9f4fcee541cffc84b0ad73a1a426ac7
Brenda Wallace <shiny@cpan.org>
2014-06-17 15:00:37 +1200

    Changes to finding the date

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ import subprocess
 import os
 import time
 import datetime
+import dateutil.parser
 
 logger = logging.getLogger('process')
 logger.setLevel(logging.DEBUG)
@@ -36,7 +37,7 @@ def get_content(full_filename):
 	    return
     except Exception, e:
 	logger.error("Unable to read contents from {filename}".format(filename=full_filename))
-	logger.exception(e)
+	logger.error(e)
     
 
 def process_file(root, filename):
@@ -62,40 +63,85 @@ def process_file(root, filename):
     body = {"filename": full_filename, 
 	    #"contents": contents,
 	    }
-    if contents.find("PRESENTER:"):
-	interview_data(contents, filename)
-	
-    logger.debug(body)
+    
+    lines = contents.split("\n")
+    if not contents:
+        return
+    elif lines[0].strip() == 'NEWZTEL MONITORING SERVICE':
+        #print contents
+        pass
+    else:
+        interview_data(contents, filename)
+
 	
     try:
-	logger.debug( es.index(index="newztel-index", doc_type="test-type", id=filename, body=body))
+        logger.debug( es.index(index="newztel-index", doc_type="test-type", id=filename, body=body))
     except Exception, e:
-	print contents
-	raise
+        print contents
+        raise
 
+def get_date_from_line(line):
+    original = line
+    logger.debug(line)
+    line = line.strip()
+#     line = line.replace('.', ' ')
+#     line = line.replace(',', ' ')
+#     line = line.replace('  ', ' ')
+#     
+#     line = line.replace(' AUG ', ' AUGUST ')
+#     line = line.replace(' NOV ', ' NOVEMBER ')
+
+#     print line.split()
+    date_string = ' '.join(line.split(" ")[-3:])
+#     print '"' + date_string + '"'
+    if not date_string:
+        logger.warning("Cannot find a date. filename={filename} line={line}".format(line=line, filename=filename))
+        logger.warning(original)
+        return 
+    logger.debug("Date = {date}".format(date=date_string))
+    
+    try:
+        
+        date = dateutil.parser.parse(date_string)
+#         date = datetime.datetime.strptime(date_string, '%d %B %Y')
+#     print date
+        return date
+    except ValueError, e:
+        logger.error('Cannot get date from "{d}"'.format(d=date_string))
+        logger.error(e)
+        logger.warning(original)
+        time.sleep(1)
+
+    
 def interview_data(contents, filename):
     lines = contents.split("\n")
-    title = lines[0]
+    title = lines[0].strip()
+    
+    for i in range(0, len(lines)):
+        if not title.strip():
+            title = lines[i]
+        else:
+            break
+        
+    date = get_date_from_line(title)
+
+    if not date:
+        logger.warning('--------------- {filename} --------------------------'.format(filename=filename))
+        for i in range(0, 7 if len(lines) >=7 else len(lines)):
+            print "{i} {line}".format(line=lines[i], i=i)
+        logger.warning('-----------------------------------------')
+        print
+        print
+#         assert False
+        return
+        
     logger.debug("Title = {title}".format(title=title))
     
     body = {
-		    'title': title,
-		    'filename': filename,
-		    
-		    }
-    try:
-	date = title.split("\t")[1]
-	if not date:
-	    logger.warning("Cannot find a date. filename={filename} Title is {title}".format(title=title, filename=filename))
-	    return 
-	logger.debug("Date = {date}".format(date=date))
-
-	body['date'] = datetime.datetime.strptime(date, '%A %d %B %Y')
-    except:
-	pass
-    
-    
-
+    	    'title': title,
+    	    'filename': filename,
+    	    'date': date
+    	    }
     
     dialogue = []
     prev_line = None
@@ -104,34 +150,35 @@ def interview_data(contents, filename):
     line_no = 0
     
     for line in lines:
-	logger.debug("-----------------------------------------------")
-	logger.debug("line = " + line)
-	paragraph += ' ' + line
-	logger.debug("-----------------------------------------------")
-	logger.debug(paragraph)
-	
-	if prev_line == '':
-	    if not speaker and line.find(':') != -1:
-		speaker = line.split(':')[0]
-		logger.debug("Speaker = {speaker}".format(speaker=speaker))
-	    else:
-		speaker = ''
-	
-	if line == '':
-	    #print "**gap"
-	    if speaker and paragraph:
-		body['speaker'] = speaker
-		body['statement'] = paragraph
-		
-		item_id= '{filename}.dialogue.{line_no}'.format(filename=filename, line_no=line_no)
-		 #body
-		es.index(index='speaker-index', doc_type='speaker', id=item_id, body=body)
-		paragraph = ''
-		speaker = ''
-	      
-	   
-	prev_line = line
-	line_no += 1
+
+        logger.debug("-----------------------------------------------")
+        logger.debug("line = " + line)
+        paragraph += ' ' + line
+        logger.debug("-----------------------------------------------")
+        logger.debug(paragraph)
+        
+        if prev_line == '':
+            if not speaker and line.find(':') != -1:
+        	speaker = line.split(':')[0]
+        	logger.debug("Speaker = {speaker}".format(speaker=speaker))
+            else:
+        	speaker = ''
+        
+        if line == '':
+            #print "**gap"
+            if speaker and paragraph:
+        	body['speaker'] = speaker
+        	body['statement'] = paragraph
+        	
+        	item_id= '{filename}.dialogue.{line_no}'.format(filename=filename, line_no=line_no)
+        	 #body
+        	es.index(index='speaker-index', doc_type='speaker', id=item_id, body=body)
+        	paragraph = ''
+        	speaker = ''
+              
+           
+        prev_line = line
+        line_no += 1
 	#time.sleep(0.5)
 
     return {'title': title} #, dialogue: dialogue}
